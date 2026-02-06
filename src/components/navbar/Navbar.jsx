246cdbee7ecbd9bc0bfe999f21b7a6d55b335683
ydaqqa05 <yomnamdaqqa05@gmail.com>
2026-02-07 00:04:33 +0200

    create navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
-
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+             
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+               KASHOP
+              </Typography>
+             <Box sx={{display:{xs:'none',sm:'flex'},gap:3,alignItems:'center'}}>
+                <Link component={RouterLink} to={'/'} color="inherit" underline='none'>Home</Link>
+                <Link component={RouterLink} to={'/cart'} color="inherit" underline='none'>Cart</Link>
+                <Link component={RouterLink} to={'/login'} color="inherit" underline='none'>Login</Link>
+                <Link component={RouterLink} to={'/register'} color="inherit" underline='none'>Register</Link>
+              </Box>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2,display:{xs:'flex',sm:'none'} }}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div><Typography>
+        Home</Typography>
+        <Typography component={'h2'}>this is my home</Typography></div>
   )
 }
 
