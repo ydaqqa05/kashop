447bb413028b5dc62eb0383877795ab0fffd2673
ydaqqa05 <yomnamdaqqa05@gmail.com>
2026-04-11 23:06:41 +0300

    fix responsive of profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
-import {Box,Tabs,Tab,Typography,TextField,Button,Avatar,Paper,} from "@mui/material";
+import {
+  Box,
+  Tabs,
+  Tab,
+  Typography,
+  TextField,
+  Button,
+  Avatar,
+  Paper,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import useProfile from "../../hooks/useProfile";
 import Footer1 from "../../components/footer/Footer1";
 import Footer2 from "../../components/footer/Footer2";
 import useForgetPassword from "../../hooks/useForgetPassword";
-import useResetPassword from "../../hooks/useResetPassword";
 import { useNavigate } from "react-router-dom";
 import useChangePassword from "../../hooks/useChangePassword";
 import useChangeEmail from "../../hooks/useEmailPassword";
-import useOrders from "../../hooks/useOrders";
 
 function TabPanel({ children, value, index }) {
   return value === index && <Box sx={{ mt: 3 }}>{children}</Box>;
@@ -17,138 +26,138 @@ function TabPanel({ children, value, index }) {
 export default function MyAccount() {
   const [tab, setTab] = useState(0);
   const { data, isLoading } = useProfile();
-  const { mutate: changePassword, isPending } = useChangePassword();
+  const { mutate: changePassword } = useChangePassword();
   const { mutate: changeEmail } = useChangeEmail();
-  
-  const navigate=useNavigate()
-const { mutate: sendCode } = useForgetPassword();
+  const { mutate: sendCode } = useForgetPassword();
+  const navigate = useNavigate();
 
-const [form, setForm] = useState({
-  firstName: "",
-  lastName: "",
-  displayName: "",
-  email: "",
-  oldPassword: "",
-  newPassword: "",
-  confirmPassword: "",
-});
-const handleChange = (e) => {
-  setForm({
-    ...form,
-    [e.target.name]: e.target.value,
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    displayName: "",
+    email: "",
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
   });
-};
-useEffect(() => {
-  if (data) {
-    setForm({
-      firstName: data.fullName?.split(" ")[0] || "",
-      lastName: data.fullName?.split(" ")[1] || "",
-      displayName: data.userName || "",
-      email: data.email || "",
-      oldPassword: "",
-      newPassword: "",
-      confirmPassword: "",
-    });
-  }
-}, [data]);
-const handleSubmit = () => {
-  if (form.newPassword !== form.confirmPassword) {
-    alert("Passwords do not match");
-    return;
-  }
-  if (form.email !== data.email) {
-    changeEmail(form.email, {
-      onSuccess: () => {
-        alert("Email updated successfully");
-      },
-      onError: () => {
-        alert("Failed to update email");
-      },
-    });
-  }
-  if (form.newPassword) {
-    sendCode(form.email, {
-      onSuccess: () => {
-        const code = prompt("Enter code sent to your email");
 
-        changePassword(
-          {
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    if (data) {
+      setForm({
+        firstName: data.fullName?.split(" ")[0] || "",
+        lastName: data.fullName?.split(" ")[1] || "",
+        displayName: data.userName || "",
+        email: data.email || "",
+        oldPassword: "",
+        newPassword: "",
+        confirmPassword: "",
+      });
+    }
+  }, [data]);
+
+  const handleSubmit = () => {
+    if (form.newPassword !== form.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    if (form.email !== data.email) {
+      changeEmail(form.email);
+    }
+
+    if (form.newPassword) {
+      sendCode(form.email, {
+        onSuccess: () => {
+          changePassword({
             CurrentPassword: form.oldPassword,
             NewPassword: form.newPassword,
             ConfirmNewPassword: form.confirmPassword,
-          },
-          {
-            onSuccess: () => {
-              alert("Password changed successfully");
-              localStorage.removeItem("accessToken");
-              navigate("/login");
-            },
-            onError: () => {
-              alert("Failed to change password");
-            },
-          }
-        );
-      },
-    });
-};
-}
+          });
+        },
+      });
+    }
+    navigate('/login')
+  };
 
-  console.log(data)
   return (
     <>
-    <Box sx={{ display: "flex", gap: 5, p: 5, bgcolor: "#fff", minHeight: "100vh" }}>
-      <Paper sx={{ p: 3, width: 250, textAlign: "center", bgcolor: "#6F3F5F7", maxHeight: "500px" }}>
-        <Avatar
-          src="https://i.pravatar.cc/100"
-          sx={{ width: 80, height: 80, mx: "auto", mb: 2 }}
-        />
-        <Typography fontWeight={600}>{data?.fullName}</Typography>
-
-        <Tabs
-          orientation="vertical"
-          value={tab}
-          onChange={(e, newValue) => setTab(newValue)}
-          sx={{ mt: 3 }}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          gap: 5,
+          p: { xs: 2, md: 5 },
+          bgcolor: "#fff",
+          minHeight: "100vh",
+        }}
+      >
+        <Paper
+          sx={{
+            p: 3,
+            width: { xs: "100%", md: 250 },
+            textAlign: "center",
+            bgcolor: "#6F3F5F7",
+          }}
         >
-          <Tab label="Account" />
-          <Tab label="Address" />
-          <Tab label="Orders" />
-        
-          <Tab label="Log Out" />
-        </Tabs>
-      </Paper>
-      <Box sx={{ flex: 1 }}>
-        <Typography variant="h4" mb={3} fontSize={"54px"} fontFamily={"Poppins"} textAlign={'center'}>
-          My Account
-        </Typography>
-        <TabPanel value={tab} index={0}>
-          <Paper sx={{ p: 4 }}>
-            <Typography fontWeight={600} mb={2} fontSize={"20px"} fontFamily={"Inter"}>
-              Account Details
-            </Typography>
+          <Avatar
+            src="https://i.pravatar.cc/100"
+            sx={{ width: 80, height: 80, mx: "auto", mb: 2 }}
+          />
 
-            <Box sx={{ flex: 1 }}>
-              <Typography variant="caption" color="#6C7275" fontSize={"12px"} fontFamily={"Inter"} fontWeight={700} >FIRST NAME *</Typography>
-              <TextField fullWidth value={form.firstName} required  name="firstName" size="small" placeholder="First name" onChange={handleChange} fontFamily="Inter" sx={{ mb: 2 }} />
-            </Box>
-            <Box sx={{ flex: 1 }}>
-              <Typography variant="caption" color="#6C7275" fontSize={"12px"} fontFamily={"Inter"} fontWeight={700}>LAST NAME *</Typography>
-              <TextField fullWidth value={form.lastName} required name="lastName" size="small" placeholder="Last name" onChange={handleChange} fontFamily="Inter" sx={{ mb: 2 }} />
-            </Box>
-            <Box sx={{ flex: 1 }}>
-              <Typography variant="caption" color="#6C7275" fontSize={"12px"} fontFamily={"Inter"} fontWeight={700}>Display name *</Typography>
-              <TextField fullWidth size="small"  value={form.displayName} required name="displayName" placeholder="Display name" onChange={handleChange} fontFamily="Inter" sx={{ mb: 2 }}/>
-            <Typography fontSize={"12px"}  mb={3} fontWeight={"Italic"} fontFamily={"Inter"} color="#6C7275">This will be how your name will be displayed in the account section and in reviews</Typography>
-            </Box>
-            <Box sx={{ flex: 1 }}>
-              <Typography variant="caption" color="#6C7275" fontSize={"12px"} fontFamily={"Inter"} fontWeight={700}>Email *</Typography>
-              <TextField fullWidth size="small" value={form.email} required name="email" placeholder="Email" onChange={handleChange} fontFamily="Inter" sx={{ mb: 2 }}/>
-            </Box>
+          <Typography fontWeight={600}>{data?.fullName}</Typography>
 
-            <Typography fontWeight={600} mb={2} fontSize={"20px"} fontFamily={"Inter"}>
-              Password
-            </Typography>
-            <Box sx={{ flex: 1 }}>
+          <Box sx={{ display: { xs: "block", md: "none" }, mt: 3 }}>
+            <Select
+              fullWidth
+              value={tab}
+              onChange={(e) => setTab(e.target.value)}
+            >
+              <MenuItem value={0}>Account</MenuItem>
+              <MenuItem value={1}>Address</MenuItem>
+              <MenuItem value={2}>Orders</MenuItem>
+              <MenuItem value={3}>Log Out</MenuItem>
+            </Select>
+          </Box>
+          <Tabs
+            orientation="vertical"
+            value={tab}
+            onChange={(e, newValue) => setTab(newValue)}
+            sx={{ mt: 3, display: { xs: "none", md: "flex" } }}
+          >
+            <Tab label="Account" />
+            <Tab label="Address" />
+            <Tab label="Orders" />
+            <Tab label="Log Out" />
+          </Tabs>
+        </Paper>
+        <Box sx={{ flex: 1 }}>
+          <Typography
+            variant="h4"
+            mb={3}
+            textAlign="center"
+            fontSize={{ xs: "28px", md: "54px" }}
+          >
+            My Account
+          </Typography>
+          <TabPanel value={tab} index={0}>
+            <Paper sx={{ p: { xs: 2, md: 4 } }}>
+              <Typography fontWeight={600} mb={2}>
+                Account Details
+              </Typography>
+
+              <TextField fullWidth name="firstName" value={form.firstName} onChange={handleChange} label="First Name" sx={{ mb: 2 }} />
+              <TextField fullWidth name="lastName" value={form.lastName} onChange={handleChange} label="Last Name" sx={{ mb: 2 }} />
+              <TextField fullWidth name="displayName" value={form.displayName} onChange={handleChange} label="Display Name" sx={{ mb: 2 }} />
+              <TextField fullWidth name="email" value={form.email} onChange={handleChange} label="Email" sx={{ mb: 2 }} />
+
+              <Typography fontWeight={600} mt={3} mb={2}>
+                Password
+              </Typography>
+              <Box sx={{ flex: 1 }}>
               <Typography variant="caption" color="#6C7275" fontSize={"12px"} fontFamily={"Inter"} fontWeight={700}>Old password</Typography>
               <TextField fullWidth size="small" value={form.oldPassword} required name="oldPassword" placeholder="Old password" onChange={handleChange} fontFamily="Inter" sx={{ mb: 2 }}/>
             </Box>
@@ -161,36 +170,42 @@ const handleSubmit = () => {
               <TextField fullWidth size="small" value={form.confirmPassword} required name="confirmPassword" placeholder="Repeat new password" onChange={handleChange} fontFamily="Inter" sx={{ mb: 2 }}/>
             </Box>
 
-            <Button variant="contained"  onClick={handleSubmit} sx={{ bgcolor: "black",py:"12px",px:"40px",mt:1,borderRadius:2 }} >
-              Save changes
-            </Button>
-          </Paper>
-        </TabPanel>
+              <Button
+                variant="contained"
+                onClick={handleSubmit}
+                sx={{
+                  bgcolor: "black",
+                  mt: 2,
+                  width: { xs: "100%", md: "auto" }, 
+                }}
+              >
+                Save Changes
+              </Button>
+            </Paper>
+          </TabPanel>
+          <TabPanel value={tab} index={1}>
+            <Paper sx={{ p: { xs: 2, md: 4 } }}>
+              <Typography fontWeight={600} mb={2}>
+                Address
+              </Typography>
 
-        {/* Address Tab */}
-        <TabPanel value={tab} index={1}>
-          <Paper sx={{ p: 4 }}>
-            <Typography fontWeight={600} mb={2}>
-              Address
-            </Typography>
+              <TextField fullWidth label="Street" sx={{ mb: 2 }} />
+              <TextField fullWidth label="City" sx={{ mb: 2 }} />
+              <TextField fullWidth label="Country" sx={{ mb: 2 }} />
+              <TextField fullWidth label="Zip Code" sx={{ mb: 3 }} />
 
-            <TextField fullWidth label="Street" sx={{ mb: 2 }} />
-            <TextField fullWidth label="City" sx={{ mb: 2 }} />
-            <TextField fullWidth label="Country" sx={{ mb: 2 }} />
-            <TextField fullWidth label="Zip Code" sx={{ mb: 3 }} />
+              <Button variant="contained" sx={{ bgcolor: "black", width: { xs: "100%", md: "auto" } }}>
+                Save Address
+              </Button>
+            </Paper>
+          </TabPanel>
+          <TabPanel value={tab} index={2}>
+            <Paper sx={{ p: { xs: 2, md: 4 } }}>
+              <Typography fontWeight={600} mb={3}>
+                Orders History
+              </Typography>
 
-            <Button variant="contained" sx={{ bgcolor: "black" }}>
-              Save Address
-            </Button>
-          </Paper>
-        </TabPanel>
-        <TabPanel value={tab} index={2}>
-  <Paper sx={{ p: 4 }}>
-    <Typography fontWeight={600} mb={3} fontSize={"20px"}>
-      Orders History
-    </Typography>
-
-    <Box>
+              <Box>
       <Box
         sx={{
           display: "grid",
@@ -239,10 +254,10 @@ const handleSubmit = () => {
   })
 )}
     </Box>
-  </Paper>
-</TabPanel>
-<TabPanel value={tab} index={4}>
-  <Paper
+            </Paper>
+          </TabPanel>
+          <TabPanel value={tab} index={3}>
+          <Paper
     sx={{
       p: 5,
       textAlign: "center",
@@ -284,11 +299,12 @@ const handleSubmit = () => {
       Log Out
     </Button>
   </Paper>
-</TabPanel>
+          </TabPanel>
+        </Box>
       </Box>
-    </Box>
-   <Footer1/>
-   <Footer2/>
+
+      <Footer1 />
+      <Footer2 />
     </>
   );
 }
