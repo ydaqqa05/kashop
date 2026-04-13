b0524310d2c3ce8a88cd6c9be204f5cecc319723
ydaqqa05 <yomnamdaqqa05@gmail.com>
2026-04-13 18:55:20 +0300

    fix center of title

--- src/components/authSide/LeftSide.jsx
+++ src/components/authSide/LeftSide.jsx
@@ -4,7 +4,7 @@ import sofa from "../../assets/image/sofa.webp";
 export default function LeftSide() {
   return (
     <Box flex={1} display="flex" position={'relative'}>
-          <Typography variant="h6" fontWeight="bold" mb={4} position={'absolute'} left={'50%'} >  3legant.  </Typography>
+          <Typography variant="h6" fontWeight="bold" mb={4} position={'absolute'} left={'50%'}  sx={{ transform: "translateX(-50%)" }}  >  3legant.  </Typography>
             <Box component="img" src={sofa} alt="sofa"
               sx={{ width: "100%",height: "100vh",objectFit: "cover",}}/>
           </Box>
